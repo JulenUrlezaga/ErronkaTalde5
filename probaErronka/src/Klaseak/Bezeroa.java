d0d8bb891388d253af91bef6f289e675c0b36b5d
ikaslea <ikaslea@DESKTOP-EI1PG8F>
2023-03-24 08:40:27 +0100

    Klase bezero eta ostatua eginda

--- probaErronka/src/Klaseak/Bezeroa.java
+++ probaErronka/src/Klaseak/Bezeroa.java
@@ -1,5 +1,82 @@
 package Klaseak;
 
-public class Bezeroa {
+import java.util.Scanner;
 
+public class Bezeroa {
+	private String NAN;
+	private String Izena;
+	private String Abizenak;
+	private String Telefono;
+	private String Email;
+	private int OrdainketaMetedoa;
+
+	public Bezeroa() {
+		this.NAN = "";
+		this.Izena = "";
+		this.Abizenak = "";
+		this.Telefono = "";
+		this.Email = "";
+		this.OrdainketaMetedoa = 0;
+	}
+	public Bezeroa(String n,String iz,String ab, String te, String em, int orme) {
+		this.NAN=n;
+		this.Izena=iz;
+		this.Abizenak=ab;
+		this.Telefono=te;
+		this.Email=em;
+		this.OrdainketaMetedoa=orme;
+	}
+	public String getNAN() {
+		return NAN;
+	}
+	public void setNAN(String nAN) {
+		NAN = nAN;
+	}
+	public String getIzena() {
+		return Izena;
+	}
+	public void setIzena(String izena) {
+		Izena = izena;
+	}
+	public String getAbizenak() {
+		return Abizenak;
+	}
+	public void setAbizenak(String abizenak) {
+		Abizenak = abizenak;
+	}
+	public String getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(String telefono) {
+		Telefono = telefono;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public int getOrdainketaMetedoa() {
+		return OrdainketaMetedoa;
+	}
+	public void setOrdainketaMetedoa(int ordainketaMetedoa) {
+		OrdainketaMetedoa = ordainketaMetedoa;
+	}
+	public void PantailaratuBezero() {
+		System.out.println("NAN :"+this.NAN+", izena : "+this.Izena+", Abizenak : "+this.Abizenak+", telefonoa : "+this.Telefono+", email-a : "+this.Email+", Ordainketa : "+this.OrdainketaMetedoa);
+	}
+	public void IrakurriBezero(Scanner teklatua) {
+		System.out.println("NAN :");
+		this.NAN=teklatua.next();
+		System.out.println("Izena :");
+		this.Izena=teklatua.next();
+		System.out.println("Abizenak :");
+		this.Abizenak=teklatua.next();
+		System.out.println("Telofono zenbakia :");
+		this.Telefono=teklatua.next();
+		System.out.println("email kontua:");
+		this.Email=teklatua.next();
+		System.out.println("ordainketa kontua zenbakia");
+		this.OrdainketaMetedoa=teklatua.nextInt();
+	}
 }
--- probaErronka/src/Klaseak/Ostatua.java
+++ probaErronka/src/Klaseak/Ostatua.java
@@ -1,5 +1,58 @@
 package Klaseak;
 
-public class Ostatua {
+import java.util.Scanner;
 
+public class Ostatua {
+	private int IdOstatua;
+	private String lekua;
+	private String ostatuMota;
+
+	public Ostatua() {
+		this.IdOstatua = 0;
+		this.lekua = "";
+		this.ostatuMota = "";
+	}
+
+	public Ostatua(int id, String le, String oM) {
+		this.IdOstatua = id;
+		this.lekua = le;
+		this.ostatuMota = oM;
+	}
+
+	public int getIdOstatua() {
+		return IdOstatua;
+	}
+
+	public void setIdOstatua(int idOstatua) {
+		IdOstatua = idOstatua;
+	}
+
+	public String getLekua() {
+		return lekua;
+	}
+
+	public void setLekua(String lekua) {
+		this.lekua = lekua;
+	}
+
+	public String getOstatuMota() {
+		return ostatuMota;
+	}
+
+	public void setOstatuMota(String ostatuMota) {
+		this.ostatuMota = ostatuMota;
+	}
+
+	public void PantailaratuOstuta() {
+		System.out.println("ID :" + this.IdOstatua + ", non dago :" + this.lekua + ", Zer da :" + this.ostatuMota);
+	}
+	
+	public void IrakurriOstatua(Scanner teklatua) {
+		System.out.println("ID : ");
+		this.IdOstatua=teklatua.nextInt();
+		System.out.println("Non dago :");
+		this.lekua=teklatua.next();
+		System.out.println("Zer heraikin mota da :");
+		this.ostatuMota=teklatua.next();
+	}
 }
