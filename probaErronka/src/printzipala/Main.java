d6f96fceda6423fa84448159e1a00309fed31326
ikaslea <ikaslea@DESKTOP-EI1PG8F>
2023-05-09 12:10:29 +0200

    aa

--- probaErronka/src/printzipala/Main.java
+++ probaErronka/src/printzipala/Main.java
@@ -51,97 +51,10 @@ public class Main {
 		Boolean bgarraioa=false;
 		// Datu basetik datuak kopiatu
 
-		try {
-			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/BidaiAgentzia", "root", "");
-
-			System.out.println("Konexio zuzena");
-
-			Statement st = conexion.createStatement();
-
-			ResultSet rs1 = st.executeQuery(
-					"SELECT * FROM BidaiAgentzia.Bus b, BidaiAgentzia.Garraioa g WHERE b.ID_Garraioa=g.ID_Garraioa;");
-			while (rs1.next()) {
-				Bus b1 = new Bus(rs1.getString("geltokiak"), rs1.getInt("B_jesarlekuak"), rs1.getString("ID_Garraioa"),
-						rs1.getString("Enpresa"), rs1.getString("Jatorria"), rs1.getString("Helmuga"));
-				bus.add(b1);
-			}
-			ResultSet rs2 = st.executeQuery(
-					"SELECT * FROM BidaiAgentzia.Hegazkina h, BidaiAgentzia.Garraioa g WHERE h.ID_Garraioa=g.ID_Garraioa;");
-			while (rs2.next()) {
-				Hegazkina h1 = new Hegazkina(rs2.getInt("H_Jesarlekua"), rs2.getString("ID_Garraioa"),
-						rs2.getString("Enpresa"), rs2.getString("Jatorria"), rs2.getString("Helmuga"),
-						rs2.getString("Aireportua"));
-				hegazkina.add(h1);
-			}
-			ResultSet rs3 = st.executeQuery(
-					"SELECT * FROM BidaiAgentzia.Itsasontzia i, BidaiAgentzia.Garraioa g WHERE i.ID_Garraioa=g.ID_Garraioa;");
-			while (rs3.next()) {
-				Itsasontzia i1 = new Itsasontzia(rs3.getString("Portua"), rs3.getInt("Kamaroteak"),
-						rs3.getString("ID_Garraioa"), rs3.getString("Enpresa"), rs3.getString("Jatorria"),
-						rs3.getString("Helmuga"));
-				itsasontzia.add(i1);
-			}
-			ResultSet rs4 = st.executeQuery("SELECT * FROM BidaiAgentzia.erreserba_g;");
-			while (rs4.next()) {
-				Erreserba_G eg1 = new Erreserba_G(rs4.getString("NAN"), rs4.getString("ID_Garraioa"),
-						rs4.getDate("Irteera_data"), rs4.getDate("Helmuga_data"), rs4.getInt("Prezioa"));
-				erreserbag.add(eg1);
-			}
-			ResultSet rs5 = st.executeQuery("SELECT * FROM BidaiAgentzia.Erreserba_O;");
-			while (rs5.next()) {
-				Erreserba_O eo1 = new Erreserba_O(rs5.getString("NAN"), rs5.getString("ID_ostatua"),
-						rs5.getDate("Sarrera_data"), rs5.getDate("Irteera_data"), rs5.getInt("Prezioa"));
-				erreserbao.add(eo1);
-			}
-			ResultSet rs6 = st.executeQuery("SELECT * FROM BidaiAgentzia.Bezeroa;");
-			while (rs6.next()) {
-				Bezeroa b1 = new Bezeroa(rs6.getString("NAN"), rs6.getString("Izena"), rs6.getString("Abizenak"),
-						rs6.getString("Telefono"), rs6.getString("Email"), rs6.getInt("Ordainketa_metodoa"));
-				bezeroa.add(b1);
-			}
-			ResultSet rs7 = st.executeQuery("SELECT * FROM BidaiAgentzia.Ostatua;");
-			while (rs7.next()) {
-				Ostatua o1 = new Ostatua(rs7.getInt("ID_Ostatua"), rs7.getString("Lekua"), rs7.getString("Ostatu_mota"),
-						rs7.getString("izena"), rs7.getDouble("Izarrak"));
-				ostatu.add(o1);
-			}
-
-			st.close();
-			rs1.close();
-			rs2.close();
-			rs3.close();
-			rs4.close();
-			rs5.close();
-			rs6.close();
-			rs7.close();
-			conexion.close();
-		} catch (SQLException sqle) {
-			sqle.printStackTrace();
-			System.out.println("Konexioa ez da zuzena");
-		}
+		extracted(bezeroa, ostatu, erreserbag, erreserbao, bus, hegazkina, itsasontzia);
 		// Datu basetik datuak kopiatu
 
-		try {
-			FileInputStream fis = new FileInputStream("Fakturak.dat");
-			ObjectInputStream ois = new ObjectInputStream(fis);
-
-			while (fis.available() > 0) {
-				faktura = (Faktura) ois.readObject();
-				fakturak.add(faktura);
-			}
-
-			ois.close();
-			fis.close();
-
-		} catch (FileNotFoundException fnfe) {
-			System.out.println("Error archivo no encontrado.");
-		} catch (IOException ioe) {
-			System.out.println("Error de Entrada / Salida");
-		} catch (InputMismatchException cnfe) {
-			System.out.println("Error. Zenbaki bat sartu behar duzu");
-		} catch (ClassNotFoundException cnfe) {
-			System.out.println("Error Clase No Encontrada");
-		}
+		extracted(fakturak);
 
 		// menua
 
@@ -785,4 +698,102 @@ public class Main {
 	}
 
 }
+
+	private static void extracted(ArrayList<Faktura> fakturak) {
+		Faktura faktura;
+		try {
+			FileInputStream fis = new FileInputStream("Fakturak.dat");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+
+			while (fis.available() > 0) {
+				faktura = (Faktura) ois.readObject();
+				fakturak.add(faktura);
+			}
+
+			ois.close();
+			fis.close();
+
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("Error archivo no encontrado.");
+		} catch (IOException ioe) {
+			System.out.println("Error de Entrada / Salida");
+		} catch (InputMismatchException cnfe) {
+			System.out.println("Error. Zenbaki bat sartu behar duzu");
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println("Error Clase No Encontrada");
+		}
+	}
+
+	private static void extracted(ArrayList<Bezeroa> bezeroa, ArrayList<Ostatua> ostatu,
+			ArrayList<Erreserba_G> erreserbag, ArrayList<Erreserba_O> erreserbao, ArrayList<Bus> bus,
+			ArrayList<Hegazkina> hegazkina, ArrayList<Itsasontzia> itsasontzia) {
+		try {
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/BidaiAgentzia", "root", "");
+
+			System.out.println("Konexio zuzena");
+
+			Statement st = conexion.createStatement();
+
+			ResultSet rs1 = st.executeQuery(
+					"SELECT * FROM BidaiAgentzia.Bus b, BidaiAgentzia.Garraioa g WHERE b.ID_Garraioa=g.ID_Garraioa;");
+			while (rs1.next()) {
+				Bus b1 = new Bus(rs1.getString("geltokiak"), rs1.getInt("B_jesarlekuak"), rs1.getString("ID_Garraioa"),
+						rs1.getString("Enpresa"), rs1.getString("Jatorria"), rs1.getString("Helmuga"));
+				bus.add(b1);
+			}
+			ResultSet rs2 = st.executeQuery(
+					"SELECT * FROM BidaiAgentzia.Hegazkina h, BidaiAgentzia.Garraioa g WHERE h.ID_Garraioa=g.ID_Garraioa;");
+			while (rs2.next()) {
+				Hegazkina h1 = new Hegazkina(rs2.getInt("H_Jesarlekua"), rs2.getString("ID_Garraioa"),
+						rs2.getString("Enpresa"), rs2.getString("Jatorria"), rs2.getString("Helmuga"),
+						rs2.getString("Aireportua"));
+				hegazkina.add(h1);
+			}
+			ResultSet rs3 = st.executeQuery(
+					"SELECT * FROM BidaiAgentzia.Itsasontzia i, BidaiAgentzia.Garraioa g WHERE i.ID_Garraioa=g.ID_Garraioa;");
+			while (rs3.next()) {
+				Itsasontzia i1 = new Itsasontzia(rs3.getString("Portua"), rs3.getInt("Kamaroteak"),
+						rs3.getString("ID_Garraioa"), rs3.getString("Enpresa"), rs3.getString("Jatorria"),
+						rs3.getString("Helmuga"));
+				itsasontzia.add(i1);
+			}
+			ResultSet rs4 = st.executeQuery("SELECT * FROM BidaiAgentzia.erreserba_g;");
+			while (rs4.next()) {
+				Erreserba_G eg1 = new Erreserba_G(rs4.getString("NAN"), rs4.getString("ID_Garraioa"),
+						rs4.getDate("Irteera_data"), rs4.getDate("Helmuga_data"), rs4.getInt("Prezioa"));
+				erreserbag.add(eg1);
+			}
+			ResultSet rs5 = st.executeQuery("SELECT * FROM BidaiAgentzia.Erreserba_O;");
+			while (rs5.next()) {
+				Erreserba_O eo1 = new Erreserba_O(rs5.getString("NAN"), rs5.getString("ID_ostatua"),
+						rs5.getDate("Sarrera_data"), rs5.getDate("Irteera_data"), rs5.getInt("Prezioa"));
+				erreserbao.add(eo1);
+			}
+			ResultSet rs6 = st.executeQuery("SELECT * FROM BidaiAgentzia.Bezeroa;");
+			while (rs6.next()) {
+				Bezeroa b1 = new Bezeroa(rs6.getString("NAN"), rs6.getString("Izena"), rs6.getString("Abizenak"),
+						rs6.getString("Telefono"), rs6.getString("Email"), rs6.getInt("Ordainketa_metodoa"));
+				bezeroa.add(b1);
+			}
+			ResultSet rs7 = st.executeQuery("SELECT * FROM BidaiAgentzia.Ostatua;");
+			while (rs7.next()) {
+				Ostatua o1 = new Ostatua(rs7.getInt("ID_Ostatua"), rs7.getString("Lekua"), rs7.getString("Ostatu_mota"),
+						rs7.getString("izena"), rs7.getDouble("Izarrak"));
+				ostatu.add(o1);
+			}
+
+			st.close();
+			rs1.close();
+			rs2.close();
+			rs3.close();
+			rs4.close();
+			rs5.close();
+			rs6.close();
+			rs7.close();
+			conexion.close();
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+			System.out.println("Konexioa ez da zuzena");
+		}
+	}
 }
